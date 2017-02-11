1ac43f2638cd2620fb361fa6b18884caaaf0540b
German Rafael Gomez Urbina <grgomezu@gmail.com>
2017-02-11 09:20:05 -0700

    Implemented regex parsing of options and assigned values of matches

--- getoptssharp/getoptssharp/getoptssharp.cs
+++ getoptssharp/getoptssharp/getoptssharp.cs
@@ -9,6 +9,7 @@
  */
 
 using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,20 +19,31 @@ namespace getoptssharp
 {
     public class getoptssharp
     {
-        public struct option
-        {
-            string flag;
-            string value;
-        }
 
-        public static option opt;
-        public static string arg;
-
-        private static int index = 0;
+        private static string _flag = string.Empty;
+        private static string _value = string.Empty;
+        private static string _arg = string.Empty;
+        private static int _index = 0;
+        private static int _opts_num = 0;
         private const string opts_pattern = @"\S:?";
         // TODO
         private const string args_pattern = @"";
+
         
+        public static string flag
+        {
+            get { return _flag; }
+        }
+
+        public static string value
+        {
+            get { return _value; }
+        }
+
+        public static string arg
+        {
+            get { return _arg; }
+        }     
 
           
         /// <summary>
@@ -40,18 +52,48 @@ namespace getoptssharp
         /// </summary>
         /// <param name="args">String of arguments</param>
         /// <param name="options">String of options in the form of "abc:d:e"</param>
-        public bool getopts(string args, string opts)
+        public static bool getopts(string[] args, string opts)
         {
 
-            if (string.IsNullOrEmpty(args) || string.IsNullOrEmpty(options))
+            if (args.Length == 0 || string.IsNullOrEmpty(opts))
             {
                 // There must be arguments and options
                 return false;
             }
 
             // TODO:
-            // 1. Regex each option
+            // 2. In case of error during parsing use a special
+            //  value to let user know something went wrong 
+            //  avoiding throwing errors and things
+            //  and the user can check an internal variable 
+            //  to determine what went wrong.
+            // 3. Create an overloaded getopts that supports long options 
+            // In the form of --help, --blah in a list and will be matched 
+            // to the opts variable by order.
+            Regex rgx = new Regex(opts_pattern, RegexOptions.IgnoreCase);
+            MatchCollection opts_matches = rgx.Matches(opts);
+            _opts_num = opts_matches.Count;
 
+            if (_opts_num > 0 && _index < args.Length)
+            {
+                string arg = args[_index++];
+
+                foreach (Match match in opts_matches)
+                {
+                    // the delimeter ':' is absolute, but add flag delimeter private variable 
+                    // to modify '-' to any other character (e.g.: '/')
+                    if (match.Value.TrimEnd(':') == arg.TrimStart('-'))
+                    {
+                        _flag = arg;
+                        break;
+                    } 
+                }
+            }
+            else
+            {
+                // No matches, return false
+                return false;
+            }
 
             return true;
         }
