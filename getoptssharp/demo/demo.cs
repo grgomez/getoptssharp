19bdc6d45aec57d1591fd10df4d2a6561419f33f
German Rafael Gomez Urbina <grgomezu@gmail.com>
2017-02-11 09:19:36 -0700

    Demo of library

---
+++ getoptssharp/demo/demo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    using getoptssharp;
+
+    class demo
+    {
+        static void Main(string[] args)
+        {
+
+            while(getoptssharp.getopts(args, "a:bc:d"))
+            {
+                switch (getoptssharp.flag)
+                {
+                    case "-a":
+                        Console.WriteLine("I got a");
+                        break;
+                    case "-b":
+                        Console.WriteLine("I got b");
+                        break;
+                    case "-c":
+                        Console.WriteLine("I got c");
+                        Console.WriteLine("I got value {0}", getoptssharp.value);
+                        break;
+                    case "-d":
+                        Console.WriteLine("I got d");
+                        break;
+                    default:
+                        if (getoptssharp.value == "?")
+                        {
+                            // I will get a message
+                        } 
+                        else
+                        {
+                            // Maybe I have an argument
+                            Console.WriteLine("I got an argument {0}", getoptssharp.arg);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
